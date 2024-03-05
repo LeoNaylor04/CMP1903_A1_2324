dd5e7d5dec652efdf64ee3e6251001517a61f302
LeoNaylor04 <leonaylor04@gmail.com>
2024-03-05 20:19:48 +0000

    Changed some comments

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -8,20 +8,12 @@ namespace CMP1903_A1_2324
 {
     internal class Die
     {
-        /*
-         * The Die class should contain one property to hold the current die value,
-         * and one method that rolls the die, returns and integer and takes no parameters.
-         */
-
-        //Property
         private int D_Value = 0; // encapsulated die value
         public int value // die value able to be used by rest of program, modifying the encapsulatd value
         {
             get { return D_Value; }
             set { D_Value = value; }
         }
-
-        //Method
         public int Roll() // returns random number between 1 and 6 inclusive
         {
             Random rnd = new Random();
--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -9,14 +9,6 @@ namespace CMP1903_A1_2324
 {
     internal class Game
     {
-        /*
-         * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
-         *
-         * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
-         * rolls could be continous, and the totals and other statistics could be summarised for example.
-         */
-
-        //Methods
         private dynamic Roll_Dice() //creates a new die object and rolls it returning the value of the roll.
         {
             Die die = new Die();
